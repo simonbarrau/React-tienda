1e56869a0bf3f04bcb7c99b2c91c9382be7a08bc
simonbarrau <simonbarrau@gmail.com>
2022-08-25 18:51:19 -0300

    navega

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,21 @@
 const NavBar= () => {
     return(
-        <div>Hello World</div>
+        
+        <main className="nave">
+           
+        
+            <ul className="ul" >
+
+            <li className="leii">WAKEIT</li>
+            <li className="lei">HOME</li>
+            <li className="lei">PRODUCTS</li>
+            <li className="lei">ABOUT US</li>
+            <li className="lei">LEGENDS</li>
+
+            </ul>
+                
+                
+                </main>
     );
 }
 
